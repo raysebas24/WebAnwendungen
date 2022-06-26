f98efa114d0c2c22c58c5f5e5852e6127803b73b
raysebas24 <ray.sebastian100@gmail.com>
2022-06-26 19:46:14 +0200

    Final Commit

--- app.js
+++ app.js
@@ -7,46 +7,53 @@ app.listen(3000, () => {
 
 var path = require('path');
 
-//view engine
-app.set('view engine', 'ejs')
+try{
 
-app.use(express.static(__dirname));                         //express.static stellt statische Dateien bereit. (z.B. Bilder, CSS-Dateien, usw.)
-app.use(express.static('views'));                           //app.use registriert ein Middleware. Ich kann jetzt Dateien aus 'views' verwenden
-app.set('views', __dirname + '/views');                     //app.set speichert Eigenschaften, welche später von app.get aufgerufen werden können. Speichern und abrufen vno Variablen
-app.use(express.static('js'));
-app.set('js', __dirname + '/js');
-app.use(express.static('public'));
-app.set('public', __dirname + '/public');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/public', express.static(path.join(__dirname, 'public')));
+    //view engine
+    app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    console.log("Home page entered")
-    res.render('index')
-})
+    app.use(express.static(__dirname));                         //express.static stellt statische Dateien bereit. (z.B. Bilder, CSS-Dateien, usw.)
+    app.use(express.static('views'));                           //app.use registriert ein Middleware. Ich kann jetzt Dateien aus 'views' verwenden
+    app.set('views', __dirname + '/views');                     //app.set speichert Eigenschaften, welche später von app.get aufgerufen werden können. Speichern und abrufen vno Variablen
+    app.use(express.static('js'));
+    app.set('js', __dirname + '/js');
+    app.use(express.static('public'));
+    app.set('public', __dirname + '/public');
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use('/public', express.static(path.join(__dirname, 'public')));
 
- app.get('/index', (req, res) => {
-    console.log("Index page entered")
-    res.render('index')
-})
 
-app.get('/buchen', (req, res) => {
-    console.log("Buchen page entered")
-    res.render('buchen')
-})
 
-app.get('/betreiber', (req, res) => {
-    console.log("Betreiber page entered")
-    res.render('betreiber')
-})
+    app.get('/', (req, res) => {
+        console.log("Home page entered")
+        res.render('index')
+    })
 
-app.get('/hafen', (req, res) => {
-    console.log("Hafen page entered")
-    res.render('hafen')
-})
+    app.get('/index', (req, res) => {
+        console.log("Index page entered")
+        res.render('index')
+    })
 
-app.get('/admin', (req, res) => {
-    console.log("Admin page entered")
-    res.render('admin')
-}) 
+    app.get('/buchen', (req, res) => {
+        console.log("Buchen page entered")
+        res.render('buchen')
+    })
 
+    app.get('/betreiber', (req, res) => {
+        console.log("Betreiber page entered")
+        res.render('betreiber')
+    })
+
+    app.get('/hafen', (req, res) => {
+        console.log("Hafen page entered")
+        res.render('hafen')
+    })
+
+    app.get('/admin', (req, res) => {
+        console.log("Admin page entered")
+        res.render('admin')
+    }) 
+
+} catch(ex){
+    console.error(ex);
+}
